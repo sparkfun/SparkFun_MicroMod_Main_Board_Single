ffc102b20799c3f0b2301d237b2c11beba18b098
bboyho <ho.chan@colorado.edu>
2023-01-04 03:11:36 -0700

    Update MicroMod_Main_Board_Single_Enable_Function_Board.ino
    - update comments and notes
    - include updated GPIO pin to toggle PWR_EN0

--- Firmware/MicroMod_Main_Board_Single_Enable_Function_Board/MicroMod_Main_Board_Single_Enable_Function_Board.ino
+++ Firmware/MicroMod_Main_Board_Single_Enable_Function_Board/MicroMod_Main_Board_Single_Enable_Function_Board.ino
@@ -2,6 +2,7 @@
 
   WRITTEN BY: Ho Yun "Bobby" Chan
   @ SparkFun Electronics
+  UPDATED: 1/3/2023
   DATE: 10/19/2021
   GITHUB REPO: https://github.com/sparkfun/SparkFun_MicroMod_Main_Board_Single
   DEVELOPMENT ENVIRONMENT SPECIFICS:
@@ -17,14 +18,14 @@
   Note that this example code uses the MicroMod Main Board - Single. The MicroMod
   Main Board - Double routes the PWR_EN# pins slightly different for the
   two function boards. 
-  
+
   ========== HARDWARE CONNECTIONS ==========
   MicroMod Artemis Processor Board => MicroMod Main Board - Single => Function Board
 
   Feel like supporting open source hardware?
   Buy a board from SparkFun!
        MicroMod Artemis Processor Board - https://www.sparkfun.com/products/16401
-       MicroMod Main Board - Single - https://www.sparkfun.com/products/18575
+       MicroMod Main Board - Single - https://www.sparkfun.com/products/20748
        MicroMod Environmental Function Board - https://www.sparkfun.com/products/18632
 
   LICENSE: This code is released under the MIT License (http://opensource.org/licenses/MIT)
@@ -34,22 +35,62 @@
 /*Define the power enable pins for the processor board with either SDIO_DATA2 or A1.
   Depending on the processor board, the Arduino pin may be different. 
 
-  Note: Certain Processor Boards like the Artemis have SDIO_Data2 and A1 available
-        to control the Function Board's voltage regulator. SAMD51, ESP32, and STM32
-        Processor Board pins do not have SDIO Data 2, so users will need to reference
-        the Processor Pin A1. Below are a few examples. */
+  Note: For MicroMod Main Board - Single v2.1, there is now only one GPIO available 
+        to toggle the Power Enable 0 (PWR_EN0) pin. The previous defines for a few 
+        Processor Boards are still available for those that have v1.1.
+
+        For MicroMod Main Board - Single v1.1, certain Processor Boards like the 
+        Artemis have SDIO_DATA2 and A1 available to control the Function Board's
+        voltage regulator. SAMD51, ESP32, and STM32 Processor Board pins do not 
+        have SDIO_DATA2, so users will need to reference the Processor Pin A1.
+        Below are a few examples.*/
 
 
-//ARTEMIS
-#define PWR_EN0 4   //Function Board 0's "PWR_EN0" pin <= MicroMod SDIO_DATA2 => Artemis Processor Board (D4)
+
+//==========ARTEMIS==========
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v2.1 PWM1 => Artemis Processor Board (D45)
+#define PWR_EN0 45
+
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v1.1 SDIO_DATA2 => Artemis Processor Board (D4)
+//#define PWR_EN0 4
+
 //Alternative option that does the same thing. Make sure to just choose one for PWR_EN0
-//#define PWR_EN0 35   //Function Board 0's "PWR_EN0" pin <= MicroMod A1 => Artemis Processor Board (A35)
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v1.1 A1 => Artemis Processor Board (A35)
+//#define PWR_EN0 35
 
-//TEENSY
-//#define PWR_EN0 15   //Function Board 0's "PWR_EN0" pin <= MicroMod A1 => Teensy Processor Board (A1)
 
-//SAMD51
-//#define PWR_EN0 18   //Function Board 0's "PWR_EN0" pin <= MicroMod A1 => SAMD51 Processor Board (18)
+
+//==========TEENSY==========
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main- Single v2.1 PWM1 => Teensy Processor Board (2)
+//#define PWR_EN0 2
+
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v1.1 SDIO_DATA2 => Teensy Processor Board (39)
+//#define PWR_EN0 39
+
+//Alternative option that does the same thing. Make sure to just choose one for PWR_EN0
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v1.1 A1 => Teensy Processor Board (A1)
+//#define PWR_EN0 15
+
+
+
+//Note: For those using Main - Single v1.1;
+//      SAMD51, ESP32, and STM32 Processor Board Pins
+//      do not have SDIO_DATA2 and SDIO_DATA1.
+
+//==========SAMD51==========
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v2.1 PWM1 => SAMD51 Processor Board (20)
+//#define PWR_EN0 20
+
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v1.1 A1 => SAMD51 Processor Board (18)
+//#define PWR_EN0 18
+
+
+//==========ESP32==========
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v2.1 PWM1 => ESP32 Processor Board (12)
+//#define PWR_EN0 12
+
+//Function Board 0's "PWR_EN0" pin <= MicroMod Main - Single v1.1 A1 => ESP32 Processor Board (35)
+//#define PWR_EN0 35
 
 
 
@@ -63,7 +104,7 @@ void setup() {
 void loop() {
   digitalWrite(PWR_EN0, HIGH); // turn the 3.3V regulator on (HIGH is the voltage level)
   delay(5000);                 // wait for a few seconds to do something with the function boards
-  
+
   digitalWrite(PWR_EN0, LOW);  // turn the 3.3V regulator off by making the voltage LOW
   delay(5000);                 // wait for a few seconds before turning function boards back on
 
